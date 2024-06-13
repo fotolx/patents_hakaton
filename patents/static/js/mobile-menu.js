df2d90258de7319771e053e8c9ed9042370e7b7f
fotolx <63285940+fotolx@users.noreply.github.com>
2024-06-13 20:13:53 +0300

    Delete odd files

--- patents/static/js/mobile-menu.js
+++
@@ -1,6 +0,0 @@
-const burgerMemu = document.querySelector('[data-burger]');
-const mainNavigationMobile = document.querySelector('.main-navigation');
-
-burgerMemu.addEventListener('click', () => {
-    mainNavigationMobile.classList.toggle('mainNavigation--mobile')
-})
